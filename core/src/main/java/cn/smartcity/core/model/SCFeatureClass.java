ae5bd369d971139538a882a5060bc5e3946da626
lipo <1543620292@qq.com>
2019-11-27 11:27:44 +0800

    1、根据相关ArcGIS相关资料进行完善数据模型

--- core/src/main/java/cn/smartcity/core/model/SCFeatureClass.java
+++ core/src/main/java/cn/smartcity/core/model/SCFeatureClass.java
@@ -4,6 +4,8 @@ package cn.smartcity.core.model;
  * @author lipoGiser
  * @date 2019/11/22 17:12
  * @Version 0.1
+ * @des 同类空间要素的集合即为要素类。如：河流、道路、电缆等。要素类之间可以独立存在
+ * @des 也可以具有某种关系，将不同的要素类之间存在关系时，将其组织到一个要素数据集（Feature dataset）中
  */
 public class SCFeatureClass {
     private Long id;
---
+++ core/src/main/java/cn/smartcity/core/model/SCFeatureDataSet.java
@@ -0,0 +1,14 @@
+package cn.smartcity.core.model;
+
+/**
+ * @author lipoGiser
+ * @date 2019/11/22 17:18
+ * @Version 0.1
+ * 要素数据集，是由一组具有相同空间参照的要素类组织
+ * 将不同的要素类放到一个要素数据集下一般有三种情况：
+ * （1）专题归类表示——当不同的要素类属于同一范畴。
+ * （2）创建几何网络——在同一几何网络中充当连接点和边的各种要素类，须组织到同一要素数据集中。。
+ * （3）考虑平面拓扑（Planar topologies）——共享公共几何特征的要素类。
+ */
+public class SCFeatureDataSet {
+}
