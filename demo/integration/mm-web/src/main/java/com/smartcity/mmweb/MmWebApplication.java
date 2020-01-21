10cc71ff70790a7fe0e8bf1248256897c55ec935
lipo <1543620292@qq.com>
2020-01-21 09:41:41 +0800

    1、增加多模块demo

---
+++ demo/integration/mm-web/src/main/java/com/smartcity/mmweb/MmWebApplication.java
@@ -0,0 +1,13 @@
+package com.smartcity.mmweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmWebApplication.class, args);
+    }
+
+}
